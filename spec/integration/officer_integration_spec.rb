83c7a10e91bffb77385b8ac1f390aa420468d42e
root <root@localhost>
2021-10-03 22:08:15 +0000

    completed CRUD test for officer

--- spec/integration/officer_integration_spec.rb
+++ spec/integration/officer_integration_spec.rb
@@ -52,10 +52,7 @@ end
 
 RSpec.describe 'Destroy officer', type: :feature do
   let!(:officer) { Officer.create(officer_id: '631009798', name: 'Yue Hu', email:'yueh@tamu.edu', amountOwed:0) }
-
-
   scenario 'successfully' do
-    
     # An officer has been created
     expect(Officer.count).to eq(1)
     visit officers_path
@@ -69,10 +66,10 @@ RSpec.describe 'Destroy officer', type: :feature do
   end
 end
 
+
+
 RSpec.describe 'Edit officer', type: :feature do
   let!(:officer) { Officer.create(officer_id: '631009798', name: 'Yue Hu', email:'yueh@tamu.edu', amountOwed:0) }
-
-
   scenario 'successfully' do
     
     # An officer has been created
@@ -83,21 +80,52 @@ RSpec.describe 'Edit officer', type: :feature do
     expect(page).to have_content("0")
     expect(page).to have_content("yueh@tamu.edu") 
     click_on 'Edit'
-    fill_in 'Officer UIN', with: 631009098
+    fill_in 'Officer UIN', with: 631009798
     fill_in 'Name', with: 'Yuan Lisha'
     fill_in 'Email', with: 'lisha@tamu.edu'
     fill_in 'Amount Owed', with: 0
     click_on 'Update Officer'
     expect(page).to have_content('Officer was successfully updated.')
-    expect(page).to have_content("631009098")
+    expect(page).to have_content("631009798")
     expect(page).to have_content("Yuan Lisha")
     expect(page).to have_content("0")
     expect(page).to have_content("lisha@tamu.edu") 
   end
 
+  scenario 'fails because of duplication' do
+    Officer.create(officer_id: 6898, name:'Lisha Yuan',email: 'lisha@tamu.edu',amountOwed:0)
 
+    expect(Officer.count).to eq(2)
+    visit officers_path
+    expect(page).to have_content("6898")
+    page.find('tr', text: '6898').click_on 'Edit'
+    fill_in 'Officer UIN', with: 631009798
+    fill_in 'Name', with: 'Yuan Lisha'
+    fill_in 'Email', with: 'lisha@tamu.edu'
+    fill_in 'Amount Owed', with: 0
+    click_on 'Update Officer'
+    expect(page).to have_content('Officer UIN has already been taken')
+  end
 end
 
+
+RSpec.describe 'Show officer', type: :feature do
+  let!(:officer) { Officer.create(officer_id: '631009798', name: 'Yue Hu', email:'yueh@tamu.edu', amountOwed:0) }
+  scenario 'successfully' do
+    # An officer has been created
+    expect(Officer.count).to eq(1)
+    visit officers_path
+    expect(page).to have_content("631009798")
+    expect(page).to have_content("Yue Hu")
+    expect(page).to have_content("0")
+    expect(page).to have_content("yueh@tamu.edu") 
+    click_on 'Show'
+    expect(page).to have_content("631009798")
+    expect(page).to have_content("Yue Hu")
+    expect(page).to have_content("0")
+    expect(page).to have_content("yueh@tamu.edu") 
+  end
+end
 
 
 
