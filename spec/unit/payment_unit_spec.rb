d36a5767b66e32a9c64bf1601df272fb9778d7b7
Jake07D <82400893+Jake07D@users.noreply.github.com>
2021-10-03 19:20:25 -0500

    updated payment unit spec

--- spec/unit/payment_unit_spec.rb
+++ spec/unit/payment_unit_spec.rb
@@ -7,13 +7,9 @@ RSpec.describe Payment, type: :model do
   email: 'example@tamu.edu', phoneNumber: '1234567890', joinDate: '1813-01-28')
   off_id_temp = Officer.create(officer_id: 999899799, name: 'Onam', email: 'e@tamu.edu',
   amountOwed: 15)
-  described_class.new(paymentId: 111, paymentMethod: 'cash', date: '1813-01-28',
+  described_class.new(paymentId: 1, paymentMethod: 'cash', date: '1813-01-28',
 	membershipType: 'one semester', membershipExpiration: '1813-01-28', amount: 15,
 	member_id: 111222333, officer_id: 999899799)
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
   end
 
   it 'is not valid without a paymentId' do
