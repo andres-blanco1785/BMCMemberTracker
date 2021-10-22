6973246242599ac774861d165590b8afe515a8bc
root <root@localhost>
2021-10-22 21:11:47 +0000

    trying to get deposits to work

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,10 +1,10 @@
 class Deposit < ApplicationRecord
   # has_many :officers, foreign_key: "officer_uin"
   # has_many :transaction_types, foreign_key: "category"
-  belongs_to :transaction_type, primary_key: "category", foreign_key: "category"
   belongs_to :officers, primary_key: "officer_uin", foreign_key: "officer_uin"
+  belongs_to :transaction_type, primary_key: "category", foreign_key: "category"
   validates :officer_uin, presence: true
-	validates :transaction_id, presence: true
+	validates :category, presence: true
   validates :amount, presence: true
   validates :date, presence: true
 end
--- app/views/deposits/_deposit.json.jbuilder
+++ app/views/deposits/_deposit.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! deposit, :id, :deposit_id, :officer_uin, :transcation_id, :amount, :date, :created_at, :updated_at
+json.extract! deposit, :id, :deposit_id, :officer_uin, :category, :amount, :date, :created_at, :updated_at
 json.url deposit_url(deposit, format: :json)
