7989d287bafa9b2f0949434a214c89b2f113a8c6
root <root@localhost>
2021-10-22 19:57:25 +0000

    changes to deposits

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,6 +1,8 @@
 class Deposit < ApplicationRecord
-  has_many :officers, foreign_key: "officer_uin"
-  has_many :transaction_types, foreign_key: "category"
+  # has_many :officers, foreign_key: "officer_uin"
+  # has_many :transaction_types, foreign_key: "category"
+  belongs_to :transaction_type, primary_key: "category", foreign_key: "category"
+  belongs_to :officers, primary_key: "officer_uin", foreign_key: "officer_uin"
   validates :officer_uin, presence: true
 	validates :transaction_id, presence: true
   validates :amount, presence: true
