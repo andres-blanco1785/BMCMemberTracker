b459950c640541c2a76d27726cba9f2362603f0f
root <root@localhost>
2021-10-22 03:15:12 +0000

    adding validations

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,6 +1,8 @@
 class Deposit < ApplicationRecord
+  belongs_to :officer, :foreign_key => 'officer_uin', :primary_key => 'officer_uin'
+  belongs_to :transaction_type, :foreign_key => 'transaction_num', :primary_key => 'transaction_num'
   validates :officer_uin, presence: true
 	validates :transaction_id, presence: true
   validates :amount, presence: true
-  validates :date, prsence: true
+  validates :date, presence: true
 end
