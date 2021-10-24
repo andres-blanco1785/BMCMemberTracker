bde09e6eb26288bf1333c5fe09791b1d2215ae01
root <root@localhost>
2021-10-24 23:08:27 +0000

    link officer to deposit and withdrawal

--- app/models/transaction_type.rb
+++ app/models/transaction_type.rb
@@ -1,6 +1,6 @@
 class TransactionType < ApplicationRecord
     validates :category, presence: true, uniqueness: true
     self.primary_key = "category"
-    has_many :deposits, foreign_key: "deposit_id"
-    has_many :withdrawals, foreign_key: "withdrawal_id"
+    has_many :deposits, foreign_key:"category"
+    has_many :withdrawals, foreign_key:"category"
 end
