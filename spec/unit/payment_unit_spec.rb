863ca46de45469ff132a47ecc32638f5f9dada54
andres-blanco1785 <60301059+andres-blanco1785@users.noreply.github.com>
2021-10-25 18:41:55 -0500

    Resolve Unit Test Merge Conflict

--- spec/unit/payment_unit_spec.rb
+++ spec/unit/payment_unit_spec.rb
@@ -2,16 +2,10 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  # need to pass google sign in to get to payment tests
-  before do
-    Rails.application.env_config['devise.mapping'] = Devise.mappings[:admin]
-    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-  end
-	
   subject do
-  mem_id_temp = Member.create(member_uin: 111222333, first_name: 'Fnam', last_name: 'Lnam',
-  email: 'ja7davis@gmail.com', phone_number: '1234567890', join_date: '1813-01-28')
-  off_id_temp = Officer.create(officer_uin: 999899799, name: 'Onam', email: 'ja7davis@tamu.edu',
+  mem_uin_temp = Member.create(member_uin: 111222333, first_name: 'Fnam', last_name: 'Lnam',
+  email: 'example@tamu.edu', phone_number: '1234567890', join_date: '1813-01-28')
+  off_uin_temp = Officer.create(officer_uin: 999899799, name: 'Onam', email: 'e@tamu.edu',
   amount_owed: 15)
   described_class.new(payment_id: 111, payment_mtd: 'cash', date: '1813-01-28',
 	membership_type: 'one semester', membership_expiration: '1813-01-28', amount: 15,
