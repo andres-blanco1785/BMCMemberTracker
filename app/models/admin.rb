656e561db3e1e4217783ec75d2829f0bd90c914b
justin-yoo <justin.yoo@tamu.edu>
2021-11-15 00:00:16 -0600

    updating code for rubocop requirements

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,7 +6,7 @@ class Admin < ApplicationRecord
   def self.from_google(email:, full_name:, uid:, avatar_url:)
     # return nil unless /@gmail.com || @tamu.edu\z/.match?(email)
     # return nil unless /justin.yoo@tamu.edu || test@tamu.edu\z/.match?(email)
-    return nil unless ((Officer.where(email: email).exists?(conditions = :none)) or (/test@tamu.edu\z/.match?(email)))
+    return nil unless Officer.where(email: email).exists? || /test@tamu.edu\z/.match?(email)
 
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
