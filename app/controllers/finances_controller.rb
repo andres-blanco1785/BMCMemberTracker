c8cc5c5a44c2ca4f7b53ec0b62efa91e7e01c3d6
Jacob <ja7davis@gmail.com>
2021-11-14 22:49:25 -0600

    fixed code style issues to pass rubocop

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,4 +1,3 @@
 class FinancesController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   resources :payment_methods
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # The finances page does not have model, it is an extra page... would be similar for tutorial/documentation
-  get "finances", to: "finances#index"
+  get 'finances', to: 'finances#index'
 end
