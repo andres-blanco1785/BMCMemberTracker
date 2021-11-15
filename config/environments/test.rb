76ea39c0141a0d54765e1cbee0bf1ed4de6187f9
justin-yoo <justin.yoo@tamu.edu>
2021-11-14 22:29:11 -0600

    adding updates for mock OAuth object

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # have mock information for OmniAuth
   OmniAuth.config.test_mode = true
   OmniAuth.config.allowed_request_methods += %i[get]
-  OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({ provider: 'google_oauth2', uid: '123456789', info: { name: 'John Doe', email: 'johndoe@example.com', first_name: 'John', last_name: 'Doe', image: 'https://lh3.googleusercontent.com/url/photo.jpg' }, credentials: { token: 'token', refresh_token: 'another_token', expires_at: 1_354_920_555, expires: true } })
+  OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({ provider: 'google_oauth2', uid: '123456789', info: { name: 'John Doe', email: 'test@tamu.edu', first_name: 'John', last_name: 'Doe', image: 'https://lh3.googleusercontent.com/url/photo.jpg' }, credentials: { token: 'token', refresh_token: 'another_token', expires_at: 1_354_920_555, expires: true } })
 
   # payment email seems to work w/o, but error "cannot render from ..."
   # config.web_console.permissions = '172.17.0.1'
