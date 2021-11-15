c2523201f638bbd2c9ce2fa8f7d9d0a6393111d5
Jacob <ja7davis@gmail.com>
2021-11-14 18:40:05 -0600

    fix code style issues to pass rubocop

--- spec/integration/payment_integration_spec.rb
+++ spec/integration/payment_integration_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe 'Payments Features', type: :feature do
     visit new_payment_path
     click_on 'Create Payment'
     # expect(page.has_content?('Member must exist')).to be(true)
-	# this is deleted because the member already be selected by default
+    # this is deleted because the member already be selected by default
     # expect(page).to have_content("Officer must exist")
     # this is deleted beacuse the officer already be selected by default
     expect(page.has_content?("Amount can't be blank")).to be(true)
@@ -53,8 +53,8 @@ RSpec.describe 'Payments Features', type: :feature do
     select 'December', from: 'payment_membership_expiration_2i'
     select '31', from: 'payment_membership_expiration_3i'
     fill_in 'payment_amount', with: '15'
-	# under the hood these are still UIN, but to the BMCT officers names were wanted
-	select 'Fnam Lnam, 111222333', from: 'payment_member_uin'
+    # under the hood these are still UIN, but to the BMCT officers names were wanted
+    select 'Fnam Lnam, 111222333', from: 'payment_member_uin'
     select 'Onam', from: 'payment_officer_uin'
     click_on 'Create Payment'
     expect(page.has_content?('Payment was successfully created.')).to be(true)
@@ -93,9 +93,9 @@ RSpec.describe 'Payments Features', type: :feature do
     select 'December', from: 'payment_membership_expiration_2i'
     select '31', from: 'payment_membership_expiration_3i'
     fill_in 'payment_amount', with: '15'
-	# OLD: notice changes to selects - creating payment w/valid inputs - if want to do similar to commented
+    # OLD: notice changes to selects - creating payment w/valid inputs - if want to do similar to commented
     # fill_in 'payment_member_uin', with: '111222333'
-	# select '111222333', :from => 'payment_member_uin'
+    # select '111222333', :from => 'payment_member_uin'
     # select '987654321', :from => 'payment_officer_uin'
     click_on 'Update Payment'
     expect(page.has_content?('Payment was successfully updated.')).to be(true)
